646bf9bd7bdf35e3453d9ec612da952e327b214f
sc01879 <sc01879@surrey.ac.uk>
2024-08-18 11:00:24 +0100

    train_unet.py

--- music_diffusion/evaluation.py
+++ music_diffusion/evaluation.py
@@ -1,9 +1,10 @@
 from diffusers import DDPMPipeline
+from diffusers.utils import make_image_grid
 import os
 import torch
 from music_diffusion.utils import Mel
 
-def evaluate(args, pipeline):
+def evaluate(args, epoch, pipeline):
     # Sample some images from random noise (this is the backward diffusion process).
     # The default pipeline output type is `List[PIL.Image]`
     mel = Mel()
@@ -11,8 +12,10 @@ def evaluate(args, pipeline):
         batch_size=args.eval_batch_size,
         generator=torch.Generator(device='cpu').manual_seed(55), # Use a separate torch generator to avoid rewinding the random state of the main training loop
     ).images
+    folder = os.path.join(args.output_dir, f"eval{epoch}")
+    os.makedirs(folder, exist_ok=True)
     for i,image in images:
         audio = mel.image_to_audio()
-        mel.save_audio(audio, os.path.join(args.output_dir, f"samples{i}.wav"))
-        image.save(os.path.join(args.output_dir, f"samples{i}.jpg"))
+        mel.save_audio(audio, os.path.join(folder, f"samples{i}.wav"))
+        image.save(os.path.join(folder, f"samples{i}.jpg"))
 
