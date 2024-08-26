b96f4d1dda4e135b3b9060ed5c2d5bfea282c389
sc01879 <sc01879@surrey.ac.uk>
2024-08-26 02:15:25 +0100

    interpolation script

---
+++ scripts/interpolate.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import sys
+import torch
+from diffusers import DDPMScheduler, DDIMScheduler, DDPMPipeline
+from datasets import load_dataset, load_from_disk
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from music_diffusion.evaluation import interpolation
+def main(args):
+    if args.scheduler == "ddpm":
+        noise_scheduler = DDPMScheduler(num_train_timesteps=args.train_steps)  # linear b_t [0.0001,0.02]
+    else:
+        noise_scheduler = DDIMScheduler(num_train_timesteps=args.train_steps)
+    pipeline = DDPMPipeline.from_pretrained(args.from_pretrained, scheduler=noise_scheduler)
+    pipeline.to("cuda")
+    inter_img = interpolation(args,pipeline)
+    print(type(inter_img))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img1', type=str, default=None)
+    parser.add_argument('--img2', type=str, default=None)
+    parser.add_argument('--scheduler', type=str, default='DDPM')
+    parser.add_argument('--from_pretrained', type=str, default="sebascorreia/DDPM-maestro20h")
+    parser.add_argument('--output_dir', type=str, default='./interpol')
+    parser.add_argument('--intp_type',type=str,default='slerp')
+    parser.add_argument('--lamb_val', type=float, default=0.5)
+    args = parser.parse_args()
+    if args.img1 is None or args.img2 is None:
+        raise ValueError('Enter image paths to interpolate')
+    main(args)
