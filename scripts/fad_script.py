cc71f1ae70d9f0703763cfc5e81345cc0935817f
sc01879 <sc01879@surrey.ac.uk>
2024-08-31 23:51:43 +0100

    fad_script

--- scripts/fad_script.py
+++ scripts/fad_script.py
@@ -1,11 +1,14 @@
 import argparse
 import os
 import sys
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from diffusers import DDPMScheduler, DDIMScheduler, DDPMPipeline
 from datasets import load_dataset, load_from_disk
 from music_diffusion.evaluation import evaluate, FAD
 import torch
+
+
 def main(args):
     if args.scheduler == "ddpm":
         noise_scheduler = DDPMScheduler(num_train_timesteps=args.train_steps)  # linear b_t [0.0001,0.02]
@@ -13,7 +16,7 @@ def main(args):
         noise_scheduler = DDIMScheduler(num_train_timesteps=args.train_steps)
     pipeline = DDPMPipeline.from_pretrained(args.from_pretrained, scheduler=noise_scheduler)
     pipeline.to(torch.device("cuda"))
-    fad_score = FAD(args,pipeline)
+    fad_score = FAD(args, pipeline)
     print("FAD score is: ", fad_score)
 
 
@@ -21,15 +24,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--from_pretrained', type=str, default=None)
     parser.add_argument('--output_dir', type=str, default='./eval')
-    parser.add_argument('--num_gen_img', type=int, default=500)
+    parser.add_argument('--num_gen_img', type=int, default=1000)
     parser.add_argument('--dataset', type=str, default="sebascorreia/Maestro20h")
-    parser.add_argument('--scheduler', type=str, default="ddpm")
-    parser.add_argument('--train_steps', type=int, default=1000)
-    parser.add_argument('--fad_split', type=str, default='test')
+    parser.add_argument('--scheduler', type=str, default="ddim")
+    parser.add_argument('--time_steps', type=int, default=50)
+    parser.add_argument('--fad_split', type=str, default='train')
     parser.add_argument('--fad_model',
                         type=str, default='dac',
                         choices=['dac', 'enc24', 'enc48', 'vgg'],
                         help=("Choose FAD model: dac, enc24,enc48 or vgg"), )
-
     args = parser.parse_args()
+    if args.from_pretrained is None:
+        raise ValueError("Please specify a pretrained model")
+
     main(args)
