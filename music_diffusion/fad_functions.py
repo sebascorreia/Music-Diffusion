f5e89c12d8299a77af8143299dbdaca53dad2a8a
sc01879 <sc01879@surrey.ac.uk>
2024-09-01 17:12:44 +0100

    Fixing FAD again

--- music_diffusion/fad_functions.py
+++ music_diffusion/fad_functions.py
@@ -8,6 +8,41 @@ import multiprocessing
 import fadtk
 from tqdm import tqdm
 from scipy import linalg
+PathLike = Union[str, Path]
+from hypy_utils.nlp_utils import substr_between
+from hypy_utils.tqdm_utils import pmap
+def _process_file(file: PathLike):
+    embd = np.load(file)
+    n = embd.shape[0]
+    return np.mean(embd, axis=0), np.cov(embd, rowvar=False) * (n - 1), n
+def calculate_embd_statistics_online(files: list[PathLike]) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Calculate the mean and covariance matrix of a list of embeddings in an online manner.
+
+    :param files: A list of npy files containing ndarrays with shape (n_frames, n_features)
+    """
+    assert len(files) > 0, "No files provided"
+
+    # Load the first file to get the embedding dimension
+    embd_dim = np.load(files[0]).shape[-1]
+
+    # Initialize the mean and covariance matrix
+    mu = np.zeros(embd_dim)
+    S = np.zeros((embd_dim, embd_dim))  # Sum of squares for online covariance computation
+    n = 0  # Counter for total number of frames
+
+    results = pmap(_process_file, files, desc='Calculating statistics')
+    for _mu, _S, _n in results:
+        delta = _mu - mu
+        mu += _n / (n + _n) * delta
+        S += _S + delta[:, None] * delta[None, :] * n * _n / (n + _n)
+        n += _n
+
+    if n < 2:
+        return mu, np.zeros_like(S)
+    else:
+        cov = S / (n - 1)  # compute the covariance matrix
+        return mu, cov
 
 
 def calc_frechet_distance(mu1, cov1, mu2, cov2, eps=1e-6):
