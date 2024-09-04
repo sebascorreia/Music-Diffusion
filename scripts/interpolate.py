8808ec3367c026d8912420e46465226506c6eb45
sc01879 <sc01879@surrey.ac.uk>
2024-09-04 20:41:12 +0100

    added class_labels

--- scripts/interpolate.py
+++ scripts/interpolate.py
@@ -6,16 +6,36 @@ from diffusers import DDPMScheduler, DDIMScheduler, DDPMPipeline,DDIMPipeline
 from datasets import load_dataset, load_from_disk
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from music_diffusion.evaluation import interpolation
-def main(args):
-    if args.img1 is None or args.img2 is None:
+import random
 
-        if os.path.exists(args.dataset):
-            dataset = load_from_disk(args.dataset)["train"]
-        else:
-            try:
-                dataset = load_dataset(args.dataset, split="train")
-            except Exception as e:
-                raise ValueError(f"Dataset error: {str(e)} ")
+def get_img(n,dataset):
+    if n is None:
+        rand1 = random.randint(0, len(dataset) - 1)
+        img = dataset['image'][rand1]
+    else:
+        img = dataset['image'][n]
+    return img
+def main(args):
+
+    if os.path.exists(args.dataset):
+        dataset = load_from_disk(args.dataset)["test"]
+    else:
+        try:
+            dataset = load_dataset(args.dataset, split="test")
+        except Exception as e:
+            raise ValueError(f"Dataset error: {str(e)} ")
+
+    if args.class1 is not None:
+        c1dataset = dataset.filter(lambda x : x["label"] == args.class1)
+        img1 = get_img(args.img1, c1dataset)
+    else:
+        img1 = get_img(args.img1, dataset)
+    if args.class2 is not None:
+        c2dataset = dataset.filter(lambda x : x["label"] == args.class2)
+        img2 = get_img(args.img2, c2dataset)
+    else:
+        img2 = get_img(args.img2, dataset)
+
     if args.scheduler == "ddpm":
         noise_scheduler = DDPMScheduler(num_train_timesteps=1000)  # linear b_t [0.0001,0.02]
         pipeline = DDPMPipeline.from_pretrained(args.from_pretrained, scheduler=noise_scheduler)
@@ -23,12 +43,14 @@ def main(args):
         noise_scheduler = DDIMScheduler(num_train_timesteps=1000)
         pipeline = DDIMPipeline.from_pretrained(args.from_pretrained, scheduler=noise_scheduler)
     pipeline.to("cuda")
-    inter_img = interpolation(args.img1, args.img2, pipeline= pipeline)
+    inter_img = interpolation(img1, img2, pipeline= pipeline)
     print(type(inter_img))
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--img1', type=str, default=None)
-    parser.add_argument('--img2', type=str, default=None)
+    parser.add_argument('--img1',type=int, default=None,)
+    parser.add_argument('--img2',type=int, default=None,)
+    parser.add_argument('--class1', type=str, default=None)
+    parser.add_argument('--class2', type=str, default=None)
     parser.add_argument('--scheduler', type=str, default='DDPM')
     parser.add_argument('--from_pretrained', type=str, default="sebascorreia/DDPM-maestro20h")
     parser.add_argument('--output_dir', type=str, default='./interpol')
